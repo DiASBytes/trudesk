06ee89a752662590f5e4aa3b12cb541a1c320a99
Thomas Vansteelandt <t.vansteelandt@gmail.com>
2019-04-10 11:28:11 +0200

    startup.sh

--- startup.sh
+++ startup.sh
@@ -2,12 +2,12 @@
 
 if [ ! -d /usr/src/trudesk/public/uploads/users ]; then
     echo "Creating Directory..."
-    mkdir /usr/src/trudesk/public/uploads/users
+    mkdir /root/trudesk/public/uploads/users
 fi
 
 if [ ! -f /usr/src/trudesk/public/uploads/users/defaultProfile.jpg ]; then
     echo "Coping defaultProfile.jpg"
-    cp /usr/src/trudesk/public/img/defaultProfile.jpg /usr/src/trudesk/public/uploads/users/defaultProfile.jpg
+    cp /root/trudesk/public/img/defaultProfile.jpg /usr/src/trudesk/public/uploads/users/defaultProfile.jpg
 fi
 
 node /usr/src/trudesk/runner.js
