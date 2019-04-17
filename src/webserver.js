d49353c5958dfc0fc0d72fbc9b16de199faa549d
Thomas Vansteelandt <t.vansteelandt@gmail.com>
2019-04-17 14:56:34 +0200

    Test uploads routing

--- src/webserver.js
+++ src/webserver.js
@@ -14,8 +14,8 @@
 
 var _ = require('lodash')
 var nconf = require('nconf')
-  .argv()
-  .env()
+    .argv()
+    .env()
 var async = require('async')
 var express = require('express')
 var WebServer = express()
@@ -25,133 +25,134 @@ var routes = require('./routes')
 var server = require('http').createServer(WebServer)
 var port = nconf.get('port') || 8118
 
-;(function (app) {
-  'use strict'
+    ; (function (app) {
+        'use strict'
 
-  // Load Events
-  require('./emitter/events')
+        // Load Events
+        require('./emitter/events')
 
-  module.exports.server = server
-  module.exports.app = app
-  module.exports.init = function (db, callback, p) {
-    if (p !== undefined) port = p
-    async.series(
-      [
-        function (next) {
-          var settingSchema = require('./models/setting')
-          settingSchema.getSetting('gen:timezone', function (err, setting) {
-            if (err || !setting || !setting.value) {
-              if (err) {
-                winston.warn(err)
-              }
+        module.exports.server = server
+        module.exports.app = app
+        module.exports.init = function (db, callback, p) {
+            if (p !== undefined) port = p
+            async.series(
+                [
+                    function (next) {
+                        var settingSchema = require('./models/setting')
+                        settingSchema.getSetting('gen:timezone', function (err, setting) {
+                            if (err || !setting || !setting.value) {
+                                if (err) {
+                                    winston.warn(err)
+                                }
 
-              global.timezone = 'America/New_York'
-              return next()
-            }
+                                global.timezone = 'America/New_York'
+                                return next()
+                            }
 
-            global.timezone = setting.value
+                            global.timezone = setting.value
 
-            return next()
-          })
-        },
-        function (next) {
-          middleware(app, db, function (middleware, store) {
-            module.exports.sessionStore = store
-            routes(app, middleware)
+                            return next()
+                        })
+                    },
+                    function (next) {
+                        middleware(app, db, function (middleware, store) {
+                            module.exports.sessionStore = store
+                            routes(app, middleware)
 
-            return next()
-          })
+                            return next()
+                        })
+                    }
+                ],
+                function () {
+                    return callback()
+                }
+            )
         }
-      ],
-      function () {
-        return callback()
-      }
-    )
-  }
 
-  module.exports.listen = function (callback, p) {
-    if (!_.isUndefined(p)) port = p
+        module.exports.listen = function (callback, p) {
+            if (!_.isUndefined(p)) port = p
 
-    server.on('error', function (err) {
-      if (err.code === 'EADDRINUSE') {
-        winston.error('Address in use, exiting...')
-        server.close()
-      } else {
-        winston.error(err.message)
-        throw err
-      }
-    })
+            server.on('error', function (err) {
+                if (err.code === 'EADDRINUSE') {
+                    winston.error('Address in use, exiting...')
+                    server.close()
+                } else {
+                    winston.error(err.message)
+                    throw err
+                }
+            })
 
-    server.listen(port, '0.0.0.0', function () {
-      winston.info('TruDesk is now listening on port: ' + port)
+            server.listen(port, '0.0.0.0', function () {
+                winston.info('TruDesk is now listening on port: ' + port)
 
-      if (_.isFunction(callback)) return callback()
-    })
-  }
+                if (_.isFunction(callback)) return callback()
+            })
+        }
 
-  module.exports.installServer = function (callback) {
-    var router = express.Router()
-    var controllers = require('./controllers/index.js')
-    var path = require('path')
-    var hbs = require('express-hbs')
-    var hbsHelpers = require('./helpers/hbs/helpers')
-    var bodyParser = require('body-parser')
-    var favicon = require('serve-favicon')
-    var pkg = require('../package.json')
-    var routeMiddleware = require('./middleware/middleware')(app)
+        module.exports.installServer = function (callback) {
+            var router = express.Router()
+            var controllers = require('./controllers/index.js')
+            var path = require('path')
+            var hbs = require('express-hbs')
+            var hbsHelpers = require('./helpers/hbs/helpers')
+            var bodyParser = require('body-parser')
+            var favicon = require('serve-favicon')
+            var pkg = require('../package.json')
+            var routeMiddleware = require('./middleware/middleware')(app)
 
-    app.set('views', path.join(__dirname, './views/'))
-    app.engine(
-      'hbs',
-      hbs.express3({
-        defaultLayout: path.join(__dirname, './views/layout/main.hbs'),
-        partialsDir: [path.join(__dirname, './views/partials/')]
-      })
-    )
-    app.set('view engine', 'hbs')
-    hbsHelpers.register(hbs.handlebars)
+            app.set('views', path.join(__dirname, './views/'))
+            app.engine(
+                'hbs',
+                hbs.express3({
+                    defaultLayout: path.join(__dirname, './views/layout/main.hbs'),
+                    partialsDir: [path.join(__dirname, './views/partials/')]
+                })
+            )
+            app.set('view engine', 'hbs')
+            hbsHelpers.register(hbs.handlebars)
 
-    app.use('/assets', express.static(path.join(__dirname, '../public/uploads/assets')))
+            app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')))
+            app.use('/assets', express.static(path.join(__dirname, '../public/uploads/assets')))
 
-    app.use(express.static(path.join(__dirname, '../public')))
-    app.use(favicon(path.join(__dirname, '../public/img/favicon.ico')))
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(bodyParser.json())
+            app.use(express.static(path.join(__dirname, '../public')))
+            app.use(favicon(path.join(__dirname, '../public/img/favicon.ico')))
+            app.use(bodyParser.urlencoded({ extended: false }))
+            app.use(bodyParser.json())
 
-    router.get('/healthz', function (req, res) {
-      res.status(200).send('OK')
-    })
-    router.get('/version', function (req, res) {
-      return res.json({ version: pkg.version })
-    })
+            router.get('/healthz', function (req, res) {
+                res.status(200).send('OK')
+            })
+            router.get('/version', function (req, res) {
+                return res.json({ version: pkg.version })
+            })
 
-    router.get('/install', controllers.install.index)
-    router.post('/install', routeMiddleware.checkOrigin, controllers.install.install)
-    router.post('/install/mongotest', routeMiddleware.checkOrigin, controllers.install.mongotest)
-    router.post('/install/existingdb', routeMiddleware.checkOrigin, controllers.install.existingdb)
-    router.post('/install/restart', routeMiddleware.checkOrigin, controllers.install.restart)
+            router.get('/install', controllers.install.index)
+            router.post('/install', routeMiddleware.checkOrigin, controllers.install.install)
+            router.post('/install/mongotest', routeMiddleware.checkOrigin, controllers.install.mongotest)
+            router.post('/install/existingdb', routeMiddleware.checkOrigin, controllers.install.existingdb)
+            router.post('/install/restart', routeMiddleware.checkOrigin, controllers.install.restart)
 
-    app.use('/', router)
+            app.use('/', router)
 
-    app.use(function (req, res) {
-      return res.redirect('/install')
-    })
+            app.use(function (req, res) {
+                return res.redirect('/install')
+            })
 
-    require('socket.io')(server)
+            require('socket.io')(server)
 
-    require('./sass/buildsass').buildDefault(function (err) {
-      if (err) {
-        winston.error(err)
-        return callback(err)
-      }
+            require('./sass/buildsass').buildDefault(function (err) {
+                if (err) {
+                    winston.error(err)
+                    return callback(err)
+                }
 
-      if (!server.listening) {
-        server.listen(port, '0.0.0.0', function () {
-          return callback()
-        })
-      } else {
-        return callback()
-      }
-    })
-  }
-})(WebServer)
+                if (!server.listening) {
+                    server.listen(port, '0.0.0.0', function () {
+                        return callback()
+                    })
+                } else {
+                    return callback()
+                }
+            })
+        }
+    })(WebServer)
