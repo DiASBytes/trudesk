9db0ddd50688216bb46e495e02cbf3c402304933
Thomas Vansteelandt <t.vansteelandt@gmail.com>
2019-10-29 16:30:30 +0100

    fromEmail: support@

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mailjet: {
-        fromEmail: 'support-test@diasbytes.com',
+        fromEmail: 'support@diasbytes.com',
         public: "6e87b5a60e241a7fa32c81d7b2e946b1",
         private: "0ac406dd2d8f341fb048d78857575bd8"
     }
